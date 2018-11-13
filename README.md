235497a8091a8b18a4ef167a3f0105cb9f3796e1
Mackie Underdown <macklinu@users.noreply.github.com>
2018-11-13 00:23:33 -0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# `@abide-community/cms`
+
+> The abide.community CMS source code, powered by [Netlify CMS](https://www.netlifycms.org/)
+
+## Usage
+
+> TODO
+
+## Scripts
+
+### `yarn start`
+
+Starts the development server to test the CMS locally at http://localhost:4000.
+
+### `yarn build`
+
+Builds the site to the `dist/` directory, ready to be deployed to a static site hosting service.
---
+++ src/index.ts
@@ -0,0 +1,96 @@
+interface Window {
+  CMS: object
+  initCMS(object: any): void
+}
+
+interface Field {
+  name: string
+  label?: string
+  widget?: string
+  hint?: string
+  default?: any
+  required?: boolean
+  pattern?: [string, string]
+}
+
+const sites = ['youwillbecome.org', 'cultivate.org', 'kingdomsea.org'].sort()
+
+function Blog(site) {
+  return {
+    name: `blog.${site}`,
+    label: `Blog (${site})`,
+    description: `Blog posts for ${site}`,
+    folder: `sites/${site}/blog`,
+    create: true,
+    fields: [
+      { name: 'title', label: 'Title' },
+      { name: 'date', label: 'Date', widget: 'date' },
+      { name: 'body', label: 'Body', widget: 'markdown' },
+    ],
+  }
+}
+
+function Pages(site) {
+  return {
+    label: `Pages (${site})`,
+    name: `pages.${site}`,
+    files: [
+      {
+        label: 'About Page',
+        name: 'about',
+        file: `sites/${site}/pages/about.yml`,
+        fields: [{ name: 'title', label: 'Title' }],
+      },
+    ],
+  }
+}
+
+function Info(sites) {
+  const infoFields: Field[] = [
+    {
+      name: 'address',
+      label: 'Address',
+      hint:
+        '(Optional) The address as you would like it displayed in the Contact section of the site',
+      required: false,
+    },
+    {
+      name: 'phone',
+      label: 'Phone Number',
+      hint:
+        '(Optional) The phone number as you would like it to be displayed in the Contact section of the site',
+      required: false,
+    },
+    {
+      name: 'email',
+      label: 'Email Address',
+      hint:
+        '(Optional) The email address as you would displayed in the Contact section of the site',
+      required: false,
+    },
+  ]
+  return {
+    label: `Info`,
+    name: `info`,
+    editor: { preview: false },
+    files: sites.map(site => ({
+      label: site,
+      name: site,
+      file: `sites/${site}/info.yml`,
+      fields: infoFields,
+    })),
+  }
+}
+
+window.initCMS({
+  config: {
+    backend: {
+      name: 'test-repo',
+    },
+    publish_mode: 'simple',
+    media_folder: 'static/assets',
+    collections: sites
+      .reduce((arr, site) => [...arr, Blog(site), Pages(site)], [])
+      .concat(Info(sites)),
+  },
+})
